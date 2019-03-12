2206b84c37690a30de6d4e7678fe0dba0010ad5a
Kyle Demers <kyledemers@gmail.com>
2019-03-12 13:44:21 -0400

    Add key card, key card scanners, and flashlight

---
+++ Assets/Scripts/Flashlight.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour {
+    public Light light;
+    public bool power; // 0 = off ... 1 = on
+    public float offIntensity;
+    public float onIntensity;
+
+    private float inputCooldown = 0.0f;
+    private float cooldownTime = .5f;
+    private bool isGrabbed;
+    
+    void Start() {
+        TurnOff();
+        isGrabbed = GetComponent<OVRGrabbable>().isGrabbed;
+    }
+    
+    void Update() {
+        isGrabbed = GetComponent<OVRGrabbable>().isGrabbed;
+
+        if(inputCooldown > 0.0f) {
+            inputCooldown -= Time.deltaTime;
+        } else {
+            inputCooldown = 0.0f;
+        }
+
+        if(isGrabbed && (OVRInput.Get(OVRInput.Button.One) || OVRInput.Get(OVRInput.Button.Two))) {
+            if(power) {
+                if(inputCooldown == 0.0f) {
+                    TurnOff();
+                    inputCooldown = cooldownTime;
+                }
+            } else {
+                if(inputCooldown == 0.0f) {
+                    TurnOn();
+                    inputCooldown = cooldownTime;
+                }
+            }
+        }
+    }
+
+    void TurnOff() {
+        power = false;
+        light.intensity = offIntensity;
+    }
+
+    void TurnOn() {
+        power = true;
+        light.intensity = onIntensity;
+    }
+}
---
+++ Assets/Scripts/KeyCardScanner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyCardScanner : MonoBehaviour {
+    public GameObject door;
+    public GameObject keyCard;
+
+    private float scanCooldownTime = 0.5f;
+    private float scanCooldown = 0.0f;
+
+	void OnTriggerEnter(Collider col) {
+        bool doorIsActive = door.activeSelf;
+
+        if(scanCooldown > 0.0f) {
+            scanCooldown -= Time.deltaTime;
+        } else {
+            scanCooldown = 0.0f;
+        }
+
+		if(col.gameObject.tag == keyCard.tag && scanCooldown == 0.0f) {
+            door.gameObject.SetActive(!doorIsActive);
+            scanCooldown = scanCooldownTime;
+		}
+	}
+}
