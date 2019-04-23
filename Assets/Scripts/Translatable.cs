6946b297ec7a8be602b8fabcba7fd75ebae1816a
Kyle Demers <kyledemers@gmail.com>
2019-04-23 13:04:54 -0400

    Add initial version of translator

---
+++ Assets/Scripts/Translatable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Translatable : MonoBehaviour
+{
+    public Material alienPoster;
+    public Material earthPoster;
+
+    void Start() {
+        gameObject.GetComponent<Renderer>().material = alienPoster;
+    }
+
+    public void TranslateToAlien() {
+        gameObject.GetComponent<Renderer>().material = alienPoster;
+    }
+
+    public void TranslateToEarth() {
+        gameObject.GetComponent<Renderer>().material = earthPoster;
+    }
+}
---
+++ Assets/Scripts/Translator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class Translator : MonoBehaviour {
+    public Flashlight flashlight;
+    public float radius;
+
+    private GameObject lastPosterTranslated = null;
+
+    void Update() {
+        RaycastHit hit;
+        if(flashlight.power) {
+            bool hitObject = Physics.Raycast(flashlight.transform.position, flashlight.transform.up, out hit);
+
+            if(hitObject) {
+                Debug.DrawRay(flashlight.transform.position, flashlight.transform.up, Color.white, 2);
+
+                if(hit.transform.tag == "Translatable") {
+                    lastPosterTranslated = hit.transform.gameObject;
+                    hit.transform.gameObject.GetComponent<Translatable>().TranslateToEarth();
+                } else {
+                    if(lastPosterTranslated != null) {
+                        lastPosterTranslated.GetComponent<Translatable>().TranslateToAlien();
+                    }
+                }
+            }
+        }
+    }
+}
